bioatmosphere/E3SM

ELMv2-SPRUCE changes on top of coastal main

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -147,6 +147,8 @@ module VegetationPropertiesType
      real(r8), pointer :: mbbopt(:)        => null()   !Ball-Berry stomatal conductance slope
      real(r8), pointer :: nstor(:)         => null()   !Nitrogen storage pool timescale
      real(r8), pointer :: br_xr(:)         => null()   !Base rate for excess respiration
+     real(r8), pointer :: br_mr_pft(:)     => null()   !Base rate for maintanence respiration
+     real(r8), pointer :: q10_mr_pft(:)    => null()   !Q10 for maintanence respiration
      real(r8), pointer :: crit_gdd1(:) => null()   !Deciduous pheonlogy critical GDD intercept
      real(r8), pointer :: crit_gdd2(:) => null()   !Deciduous pheonlogy critical GDD slope
      real(r8), pointer :: tc_stress        => null()   !Critial temperature for moisture stress
@@ -199,7 +201,7 @@ contains
     use pftvarcon , only : leafcp_obs, frootcp_obs, livewdcp_obs, deadwdcp_obs
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
-    use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd, crit_gdd1, crit_gdd2
+    use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, br_mr_pft, q10_mr_pft, tc_stress, lmrhd, crit_gdd1, crit_gdd2
     use pftvarcon , only : sal_threshold, KM_salinity, osm_inhib, sal_opt, sal_tol, floodf
     !
     !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
@@ -320,6 +322,8 @@ contains
     allocate( this%mbbopt(0:numpft))                             ; this%mbbopt(:)                =nan
     allocate( this%nstor(0:numpft))                              ; this%nstor(:)                 =nan
     allocate( this%br_xr(0:numpft))                              ; this%br_xr(:)                 =nan
+    allocate( this%br_mr_pft(0:numpft))                          ; this%br_mr_pft(:)             =nan
+    allocate( this%q10_mr_pft(0:numpft))                         ; this%q10_mr_pft(:)            =nan
 
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
     allocate(this%km_decomp_nh4)
@@ -432,6 +436,10 @@ contains
        this%mbbopt(m)       = mbbopt(m)
        this%nstor(m)        = nstor(m)
        this%br_xr(m)        = br_xr(m)
+#if (defined HUM_HOL)
+       this%br_mr_pft(m)    = br_mr_pft(m)
+       this%q10_mr_pft(m)   = q10_mr_pft(m)
+#endif
        this%crit_gdd1(m)    = crit_gdd1(m)
        this%crit_gdd2(m)    = crit_gdd2(m)
  

ypwong22/ELM_SPRUCE

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -146,6 +146,8 @@ module VegetationPropertiesType
      real(r8), pointer :: mbbopt(:)        => null()   !Ball-Berry stomatal conductance slope
      real(r8), pointer :: nstor(:)         => null()   !Nitrogen storage pool timescale
      real(r8), pointer :: br_xr(:)         => null()   !Base rate for excess respiration
+     real(r8), pointer :: br_mr_pft(:)     => null()   !Base rate for maintanence respiration
+     real(r8), pointer :: q10_mr_pft(:)    => null()   !Q10 for maintanence respiration
      real(r8), pointer :: crit_gdd1(:) => null()   !Deciduous pheonlogy critical GDD intercept
      real(r8), pointer :: crit_gdd2(:) => null()   !Deciduous pheonlogy critical GDD slope
      real(r8), pointer :: tc_stress        => null()   !Critial temperature for moisture stress
@@ -198,7 +200,7 @@ contains
     use pftvarcon , only : leafcp_obs, frootcp_obs, livewdcp_obs, deadwdcp_obs
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
-    use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd, crit_gdd1, crit_gdd2
+    use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, br_mr_pft, q10_mr_pft, tc_stress, lmrhd, crit_gdd1, crit_gdd2
     use pftvarcon , only : sal_threshold, KM_salinity, osm_inhib, sal_opt, sal_tol, floodf
     !
     !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
@@ -319,6 +321,8 @@ contains
     allocate( this%mbbopt(0:numpft))                             ; this%mbbopt(:)                =spval
     allocate( this%nstor(0:numpft))                              ; this%nstor(:)                 =spval
     allocate( this%br_xr(0:numpft))                              ; this%br_xr(:)                 =spval
+    allocate( this%br_mr_pft(0:numpft))                          ; this%br_mr_pft(:)             =spval
+    allocate( this%q10_mr_pft(0:numpft))                         ; this%q10_mr_pft(:)            =spval
 
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
     allocate(this%km_decomp_nh4)
@@ -431,6 +435,10 @@ contains
        this%mbbopt(m)       = mbbopt(m)
        this%nstor(m)        = nstor(m)
        this%br_xr(m)        = br_xr(m)
+#if (defined HUM_HOL)
+       this%br_mr_pft(m)    = br_mr_pft(m)
+       this%q10_mr_pft(m)   = q10_mr_pft(m)
+#endif
        this%crit_gdd1(m)    = crit_gdd1(m)
        this%crit_gdd2(m)    = crit_gdd2(m)
  