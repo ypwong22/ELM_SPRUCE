E3SM-Project/E3SM

Updates for ELM_SPRUCE

components/clm/src/data_types/ColumnDataType.F90
@@ -1,5 +1,5 @@
 module ColumnDataType
-
+    
   !-----------------------------------------------------------------------
   ! !DESCRIPTION:
   ! Column data type allocation and initialization
@@ -434,6 +434,7 @@ module ColumnDataType
     real(r8), pointer :: qflx_gross_infl_soil (:)   => null() ! gross infiltration, before considering the evaporation
     real(r8), pointer :: qflx_adv             (:,:) => null() ! advective flux across different soil layer interfaces [mm H2O/s] [+ downward]
     real(r8), pointer :: qflx_rootsoi         (:,:) => null() ! root and soil water exchange [mm H2O/s] [+ into root]     
+    real(r8), pointer :: qflx_tran_veg_sat    (:)   => null() ! Transpiration from saturated zone
     real(r8), pointer :: dwb                  (:)   => null() !  water mass change [+ increase] [mm H2O/s] 
     real(r8), pointer :: qflx_infl            (:)   => null() ! infiltration (mm H2O /s)
     real(r8), pointer :: qflx_surf            (:)   => null() ! surface runoff (mm H2O /s)
@@ -471,6 +472,10 @@ module ColumnDataType
     real(r8), pointer :: qflx_irrig           (:)   => null() ! col irrigation flux (mm H2O/s)
     real(r8), pointer :: qflx_irr_demand      (:)   => null() ! col surface irrigation demand (mm H2O /s)
     real(r8), pointer :: qflx_over_supply     (:)   => null() ! col over supplied irrigation 
+
+    real(r8), pointer :: qflx_lat_aqu         (:)   => null() ! Total lateral flux between hummock/hollow (mm H2O /s)
+    real(r8), pointer :: qflx_lat_aqu_layer   (:,:) => null() ! Lateral flux between hummock/hollow by layer (mm H2O/s)
+    real(r8), pointer :: qflx_surf_input      (:)   => null() ! Runoff input from Hummock (mm H2O/s)
 
     real(r8), pointer :: mflx_infl_1d         (:)   => null() ! infiltration source in top soil control volume (kg H2O /s)
     real(r8), pointer :: mflx_dew_1d          (:)   => null() ! liquid+snow dew source in top soil control volume (kg H2O /s)
@@ -5119,6 +5124,7 @@ contains
     allocate(this%qflx_gross_infl_soil   (begc:endc))             ; this%qflx_gross_infl_soil (:)   = nan
     allocate(this%qflx_adv               (begc:endc,0:nlevgrnd))  ; this%qflx_adv             (:,:) = nan
     allocate(this%qflx_rootsoi           (begc:endc,1:nlevgrnd))  ; this%qflx_rootsoi         (:,:) = nan
+    allocate(this%qflx_tran_veg_sat      (begc:endc))             ; this%qflx_tran_veg_sat    (:)   = nan
     allocate(this%dwb                    (begc:endc))             ; this%dwb                  (:)   = nan
     allocate(this%qflx_infl              (begc:endc))             ; this%qflx_infl            (:)   = nan
     allocate(this%qflx_surf              (begc:endc))             ; this%qflx_surf            (:)   = nan
@@ -5155,7 +5161,10 @@ contains
     allocate(this%qflx_grnd_irrig        (begc:endc))             ; this%qflx_grnd_irrig      (:)   = nan
     allocate(this%qflx_over_supply       (begc:endc))             ; this%qflx_over_supply     (:)   = nan
     allocate(this%qflx_irr_demand        (begc:endc))             ; this%qflx_irr_demand      (:)   = nan
-    
+    allocate(this%qflx_lat_aqu           (begc:endc))             ; this%qflx_lat_aqu         (:)   = nan
+    allocate(this%qflx_lat_aqu_layer     (begc:endc,1:nlevgrnd))  ; this%qflx_lat_aqu_layer   (:,:) = nan
+    allocate(this%qflx_surf_input        (begc:endc))             ; this%qflx_surf_input         (:)   = nan   
+
     !VSFM variables
     ncells = endc - begc + 1
     allocate(this%mflx_infl_1d           (ncells))                ; this%mflx_infl_1d                    (:)   = nan
@@ -5259,8 +5268,13 @@ contains
     this%qflx_snofrz(begc:endc) = spval
     call hist_addfld1d (fname='QSNOFRZ', units='kg/m2/s', &
          avgflag='A', long_name='column-integrated snow freezing rate', &
-         ptr_col=this%qflx_snofrz, set_lake=spval, c2l_scale_type='urbanf', default='inactive')
-         
+         ptr_col=this%qflx_snofrz, set_lake=spval, c2l_scale_type='urbanf', default='inactive')         
+
+    this%qflx_lat_aqu(begc:endc) = spval
+    call hist_addfld1d (fname='QLAT_AQU', units='kg/m2/s', &
+         avgflag='A', long_name='Lateral flux between hummock/hollow', &
+         ptr_col=this%qflx_lat_aqu, set_lake=spval, c2l_scale_type='urbanf', default='inactive')
+
     if (create_glacier_mec_landunit) then
        this%qflx_glcice(begc:endc) = spval
        call hist_addfld1d (fname='QICE',  units='mm/s',  &

components/clm/src/data_types/VegetationDataType.F90
@@ -104,6 +104,8 @@ module VegetationDataType
     real(r8), pointer :: begwb        (:) => null() ! water mass begining of the time step
     real(r8), pointer :: endwb        (:) => null() ! water mass end of the time step
     real(r8), pointer :: errh2o       (:) => null() ! water conservation error (mm H2O)
+    real(r8), pointer :: h2o_moss_wc    (:) => null() ! Total water content of Sphagnum moss (relative to dry mass (relative to dry mass))
+    real(r8), pointer :: h2o_moss_inter (:) => null() ! Internal water content of Sphagnum moss (relative to dry mass (relative to dry mass))
   contains
     procedure, public :: Init    => veg_ws_init
     procedure, public :: Restart => veg_ws_restart
@@ -1778,7 +1780,8 @@ module VegetationDataType
     allocate(this%begwb               (begp:endp))          ; this%begwb             (:) = nan
     allocate(this%endwb               (begp:endp))          ; this%endwb             (:) = nan
     allocate(this%errh2o              (begp:endp))          ; this%errh2o            (:) = nan
-
+    allocate(this%h2o_moss_wc         (begp:endp))          ; this%h2o_moss_wc       (:) = nan
+    allocate(this%h2o_moss_inter      (begp:endp))          ; this%h2o_moss_inter    (:) = nan
     !-----------------------------------------------------------------------
     ! initialize history fields for select members of veg_ws
     !-----------------------------------------------------------------------
@@ -1817,13 +1820,16 @@ module VegetationDataType
        call hist_addfld1d (fname='FWET', units='proportion', &
             avgflag='A', long_name='fraction of canopy that is wet', &
             ptr_patch=this%fwet, default='inactive')
-    end if
 
-    if (use_cn) then
        this%fdry(begp:endp) = spval
        call hist_addfld1d (fname='FDRY', units='proportion', &
             avgflag='A', long_name='fraction of foliage that is green and dry', &
             ptr_patch=this%fdry, default='inactive')
+
+       this%h2o_moss_wc(begp:endp) = spval
+       call hist_addfld1d (fname='H2O_MOSS_WC', units='proportion', &
+            avgflag='A', long_name='Relative water content of Moss', &
+            ptr_patch=this%h2o_moss_wc)
     end if
 
     !-----------------------------------------------------------------------

components/clm/src/dyn_subgrid/dynHarvestMod.F90
@@ -185,7 +185,7 @@ contains
     ! !USES:
     use pftvarcon       , only : noveg, nbrdlf_evr_shrub, pprodharv10
     use clm_varcon      , only : secspday
-    use clm_time_manager, only : get_days_per_year
+    use clm_time_manager, only : get_days_per_year, get_curr_date, get_step_size
     !
     ! !ARGUMENTS:
     integer                  , intent(in)    :: num_soilc       ! number of soil columns in filter
@@ -205,6 +205,7 @@ contains
     integer :: p                         ! patch index
     integer :: g                         ! gridcell index
     integer :: fp                        ! patch filter index
+    integer :: yr, mon, day, sec, dtime  ! date information
     real(r8):: am                        ! rate for fractional harvest mortality (1/yr)
     real(r8):: m                         ! rate for fractional harvest mortality (1/s)
     real(r8):: days_per_year             ! days per year
@@ -363,8 +364,18 @@ contains
       if (ivt(p) > noveg .and. ivt(p) < nbrdlf_evr_shrub) then
 
          if (do_harvest) then
-            am = harvest(g)
+           am = harvest(g)
+#if (defined HUM_HOL)
+           call get_curr_date(yr, mon, day, sec)
+           dtime  = get_step_size()
+           if (mon == 1 .and. day == 31 .and. sec == 0) then
+             m = am/dtime   !Do site-level harvest on first day of year
+           else
+             m = 0._r8
+           end if
+ #else
             m  = am/(days_per_year * secspday)
+ #endif
          else
             m = 0._r8
          end if   

ypwong22/ELM_SPRUCE

components/elm/src/data_types/ColumnDataType.F90
@@ -1,5 +1,5 @@
 module ColumnDataType
-
+    
   !-----------------------------------------------------------------------
   ! !DESCRIPTION:
   ! Column data type allocation and initialization
@@ -476,6 +476,7 @@ module ColumnDataType
     real(r8), pointer :: qflx_gross_infl_soil (:)   => null() ! gross infiltration, before considering the evaporation
     real(r8), pointer :: qflx_adv             (:,:) => null() ! advective flux across different soil layer interfaces [mm H2O/s] [+ downward]
     real(r8), pointer :: qflx_rootsoi         (:,:) => null() ! root and soil water exchange [mm H2O/s] [+ into root]
+    real(r8), pointer :: qflx_tran_veg_sat    (:)   => null() ! Transpiration from saturated zone
     real(r8), pointer :: dwb                  (:)   => null() !  water mass change [+ increase] [mm H2O/s]
     real(r8), pointer :: qflx_infl            (:)   => null() ! infiltration (mm H2O /s)
     real(r8), pointer :: qflx_surf            (:)   => null() ! surface runoff (mm H2O /s)
@@ -514,6 +515,10 @@ module ColumnDataType
     real(r8), pointer :: qflx_irr_demand      (:)   => null() ! col surface irrigation demand (mm H2O /s)
     real(r8), pointer :: qflx_over_supply     (:)   => null() ! col over supplied irrigation
     real(r8), pointer :: qflx_h2orof_drain    (:)   => null() ! drainage from floodplain inundation volume (mm H2O/s))
+
+    real(r8), pointer :: qflx_lat_aqu         (:)   => null() ! Total lateral flux between hummock/hollow (mm H2O /s)
+    real(r8), pointer :: qflx_lat_aqu_layer   (:,:) => null() ! Lateral flux between hummock/hollow by layer (mm H2O/s)
+    real(r8), pointer :: qflx_surf_input      (:)   => null() ! Runoff input from Hummock (mm H2O/s)
 
     real(r8), pointer :: mflx_infl_1d         (:)   => null() ! infiltration source in top soil control volume (kg H2O /s)
     real(r8), pointer :: mflx_dew_1d          (:)   => null() ! liquid+snow dew source in top soil control volume (kg H2O /s)
@@ -5715,6 +5720,9 @@ contains
     allocate(this%qflx_over_supply       (begc:endc))             ; this%qflx_over_supply     (:)   = spval
     allocate(this%qflx_irr_demand        (begc:endc))             ; this%qflx_irr_demand      (:)   = spval
     allocate(this%qflx_h2orof_drain      (begc:endc))             ; this%qflx_h2orof_drain    (:)   = spval
+    allocate(this%qflx_lat_aqu           (begc:endc))             ; this%qflx_lat_aqu         (:)   = spval
+    allocate(this%qflx_lat_aqu_layer     (begc:endc,1:nlevgrnd))  ; this%qflx_lat_aqu_layer   (:,:) = spval
+    allocate(this%qflx_surf_input        (begc:endc))             ; this%qflx_surf_input         (:)   = spval   
 
     !VSFM variables
     ncells = endc - begc + 1
@@ -5820,6 +5828,11 @@ contains
      call hist_addfld1d (fname='QSNOFRZ', units='kg/m2/s', &
           avgflag='A', long_name='column-integrated snow freezing rate', &
            ptr_col=this%qflx_snofrz, set_lake=spval, c2l_scale_type='urbanf', default='inactive')
+
+    this%qflx_lat_aqu(begc:endc) = spval
+    call hist_addfld1d (fname='QLAT_AQU', units='kg/m2/s', &
+         avgflag='A', long_name='Lateral flux between hummock/hollow', &
+         ptr_col=this%qflx_lat_aqu, set_lake=spval, c2l_scale_type='urbanf', default='inactive')
 
     if (create_glacier_mec_landunit) then
        this%qflx_glcice(begc:endc) = spval

components/elm/src/data_types/VegetationDataType.F90
@@ -104,6 +104,8 @@ module VegetationDataType
     real(r8), pointer :: begwb        (:) => null() ! water mass begining of the time step
     real(r8), pointer :: endwb        (:) => null() ! water mass end of the time step
     real(r8), pointer :: errh2o       (:) => null() ! water conservation error (mm H2O)
+    real(r8), pointer :: h2o_moss_wc    (:) => null() ! Total water content of Sphagnum moss (relative to dry mass (relative to dry mass))
+    real(r8), pointer :: h2o_moss_inter (:) => null() ! Internal water content of Sphagnum moss (relative to dry mass (relative to dry mass))
   contains
     procedure, public :: Init    => veg_ws_init
     procedure, public :: Restart => veg_ws_restart
@@ -1796,6 +1798,8 @@ module VegetationDataType
     allocate(this%begwb               (begp:endp))          ; this%begwb             (:) = spval
     allocate(this%endwb               (begp:endp))          ; this%endwb             (:) = spval
     allocate(this%errh2o              (begp:endp))          ; this%errh2o            (:) = spval
+    allocate(this%h2o_moss_wc         (begp:endp))          ; this%h2o_moss_wc       (:) = spval
+    allocate(this%h2o_moss_inter      (begp:endp))          ; this%h2o_moss_inter    (:) = spval
 
     !-----------------------------------------------------------------------
     ! initialize history fields for select members of veg_ws
@@ -1842,6 +1846,11 @@ module VegetationDataType
        call hist_addfld1d (fname='FDRY', units='proportion', &
             avgflag='A', long_name='fraction of foliage that is green and dry', &
             ptr_patch=this%fdry, default='inactive')
+
+       this%h2o_moss_wc(begp:endp) = spval
+       call hist_addfld1d (fname='H2O_MOSS_WC', units='proportion', &
+            avgflag='A', long_name='Relative water content of Moss', &
+            ptr_patch=this%h2o_moss_wc)
     end if
 
     !-----------------------------------------------------------------------

components/elm/src/dyn_subgrid/dynHarvestMod.F90
@@ -201,7 +201,7 @@ contains
     ! !USES:
     use pftvarcon       , only : noveg, nbrdlf_evr_shrub, pprodharv10
     use elm_varcon      , only : secspday
-    use elm_time_manager, only : get_days_per_year
+    use elm_time_manager, only : get_days_per_year, get_curr_date, get_step_size
     use GridcellType   , only : grc_pp
     
     ! !ARGUMENTS:
@@ -215,6 +215,7 @@ contains
     integer :: t,ti,topi                 ! topounit indices TKT
     integer :: g                         ! gridcell index
     integer :: fp                        ! patch filter index
+    integer :: yr, mon, day, sec, dtime  ! date information
     real(r8):: am                        ! rate for fractional harvest mortality (1/yr)
     real(r8):: m                         ! rate for fractional harvest mortality (1/s)
     real(r8):: days_per_year             ! days per year
@@ -381,7 +382,18 @@ contains
             do varnum = 1, num_harvest_vars
                am = am + harvest_rates(varnum,g)
             end do
+            am = harvest(g,ti)
+#if (defined HUM_HOL)
+           call get_curr_date(yr, mon, day, sec)
+           dtime  = get_step_size()
+           if (mon == 1 .and. day == 31 .and. sec == 0) then
+             m = am/dtime   !Do site-level harvest on first day of year
+           else
+             m = 0._r8
+           end if
+ #else
             m  = am/(days_per_year * secspday)
+ #endif
          else
             m = 0._r8
          end if   