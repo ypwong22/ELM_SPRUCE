kchenhyd/E3SM

Sulfide concentration and methane flux data from alquimia back to ELM

components/elm/src/data_types/GridcellDataType.F90
@@ -159,6 +159,7 @@ module GridcellDataType
     real(r8), pointer :: somc_yield                (:) => null()  ! (gC/m2/s) grid-level total SOM C loss by erosion
     real(r8), pointer :: DOC_runoff                (:) => null()  ! (gC/m2/s) grid-level total DOC C loss by runoff
     real(r8), pointer :: DIC_runoff                (:) => null()  ! (gC/m2/s) grid-level total DIC C loss by runoff
+    real(r8), pointer :: ch4flux                   (:) => null()  ! (gC/m2/s) grid-level total CH4 flux to atmosphere
 
   contains
     procedure, public :: Init    => grc_cf_init
@@ -704,6 +705,7 @@ contains
     allocate(this%somc_yield                   (begg:endg)) ; this%somc_yield                (:) = nan
     allocate(this%DOC_runoff                   (begg:endg)) ; this%DOC_runoff                (:) = nan
     allocate(this%DIC_runoff                   (begg:endg)) ; this%DIC_runoff                (:) = nan
+    allocate(this%ch4flux                      (begg:endg)) ; this%ch4flux                   (:) = nan
 
     !-----------------------------------------------------------------------
     ! initialize history fields for select members of grc_cf

ypwong22/ELM_SPRUCE

components/elm/src/data_types/GridcellDataType.F90
@@ -158,6 +158,7 @@ module GridcellDataType
     real(r8), pointer :: somc_yield                (:) => null()  ! (gC/m2/s) grid-level total SOM C loss by erosion
     real(r8), pointer :: DOC_runoff                (:) => null()  ! (gC/m2/s) grid-level total DOC C loss by runoff
     real(r8), pointer :: DIC_runoff                (:) => null()  ! (gC/m2/s) grid-level total DIC C loss by runoff
+    real(r8), pointer :: ch4flux                   (:) => null()  ! (gC/m2/s) grid-level total CH4 flux to atmosphere
 
   contains
     procedure, public :: Init    => grc_cf_init
@@ -702,6 +703,7 @@ contains
     allocate(this%somc_yield                   (begg:endg)) ; this%somc_yield                (:) = spval
     allocate(this%DOC_runoff                   (begg:endg)) ; this%DOC_runoff                (:) = spval
     allocate(this%DIC_runoff                   (begg:endg)) ; this%DIC_runoff                (:) = spval
+    allocate(this%ch4flux                      (begg:endg)) ; this%ch4flux                   (:) = spval
 
     !-----------------------------------------------------------------------
     ! initialize history fields for select members of grc_cf